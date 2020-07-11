bd45afde8e8388919fa112113bef8ef04ef90f46
xukee <oncwnuBDYPJsFlWf_CezM2n3uqDU@git.weixin.qq.com>
2020-07-11 11:14:33 +0800

    首页数据通过云函数调取

--- cloudfunctions/getPlaylist/index.js
+++ cloudfunctions/getPlaylist/index.js
@@ -2,6 +2,7 @@
 const cloud = require('wx-server-sdk')
 const rp = require('request-promise');
 
+const MAX_LIMIT = 10
 
 cloud.init({
   env: 'xukee-test-9iprg'
@@ -16,6 +17,27 @@ const playlistCollection = db.collection('playlist')
 // 云函数入口函数
 exports.main = async (event, context) => {
 
+  //const list = await playlistCollection.get()
+
+  const countResult = await playlistCollection.count()
+  const total = countResult.total
+  const batchTimes = Math.ceil(total / MAX_LIMIT)
+  const tasks = []
+  for (let i = 0; i < batchTimes; i++) {
+    let promise = playlistCollection.skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+    tasks.push(promise)
+  }
+  let list = {
+    data: []
+  }
+  if (tasks.length > 0) {
+    list = (await Promise.all(tasks)).reduce((acc, cur) => {
+      return {
+        data: acc.data.concat(cur.data)
+      }
+    })
+  }
+
   const playList = await rp(url).then((res) => {
     return JSON.parse(res).result
   })
@@ -23,7 +45,6 @@ exports.main = async (event, context) => {
   //console.log(playList);
   //return playList
 
-  const list = await playlistCollection.get()
   const newData = []
 
   for (let i = 0, len1 = playList.length; i < len1; i++) {
@@ -54,7 +75,7 @@ exports.main = async (event, context) => {
 
   console.log(playList.length);
 
-  console.log(list.length);
+  console.log(list.data.length);
 
   return newData.length
 
