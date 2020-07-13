b4f7a780e62685d850fad7928fc2f2307078c8b2
xukee <oncwnuBDYPJsFlWf_CezM2n3uqDU@git.weixin.qq.com>
2020-07-13 15:42:58 +0800

    进度条

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,165 @@
+// components/progress-bar/progress-bar.js
+let movableAreaWidth = 0
+let movableViewWidth = 0
+const backgroundAudioManager = wx.getBackgroundAudioManager()
+let currentSec = -1 
+let duration = 0 
+let isMoving = false 
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showTime: {
+      currentTime: '00:00',
+      totalTime: '00:00',
+    },
+    movableDis: 0,
+    progress: 0,
+  },
+
+  lifetimes: {
+    ready() {
+      if (this.properties.isSame && this.data.showTime.totalTime == '00:00') {
+        this._setTime()
+      }
+      this._getMovableDis()
+      this._bindBGMEvent()
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    onChange(event) {
+      // 拖动
+      if (event.detail.source == 'touch') {
+        this.data.progress = event.detail.x / (movableAreaWidth - movableViewWidth) * 100
+        this.data.movableDis = event.detail.x
+        isMoving = true
+      }
+    },
+
+    onTouchEnd() {
+      const currentTimeFmt = this._dateFormat(Math.floor(backgroundAudioManager.currentTime))
+      this.setData({
+        progress: this.data.progress,
+        movableDis: this.data.movableDis,
+        ['showTime.currentTime']: currentTimeFmt.min + ':' + currentTimeFmt.sec
+      })
+      backgroundAudioManager.seek(duration * this.data.progress / 100)
+      isMoving = false
+    },
+
+    _getMovableDis() {
+      const query = this.createSelectorQuery()
+      //获取组件的宽度
+      query.select('.movable-area').boundingClientRect()
+      query.select('.movable-view').boundingClientRect()
+      query.exec(rect => {
+        movableAreaWidth = rect[0].width
+        movableViewWidth = rect[1].width
+        console.log(movableAreaWidth,movableViewWidth);
+      })
+    },
+
+    _bindBGMEvent() {
+      backgroundAudioManager.onPlay(() => {
+        console.log('onPlay')
+        isMoving = false
+        this.triggerEvent('musicPlay')
+      })
+
+      backgroundAudioManager.onStop(() => {
+        console.log('onStop')
+      })
+
+      backgroundAudioManager.onPause(() => {
+        console.log('Pause')
+        this.triggerEvent('musicPause')
+      })
+
+      backgroundAudioManager.onWaiting(() => {
+        console.log('onWaiting')
+      })
+
+      backgroundAudioManager.onCanplay(() => {
+        console.log('onCanplay')
+        // console.log(backgroundAudioManager.duration)
+        if (typeof backgroundAudioManager.duration != 'undefined') {
+          this._setTime()
+        } else {
+          setTimeout(() => {
+            this._setTime()
+          }, 1000)
+        }
+      })
+
+      backgroundAudioManager.onTimeUpdate(() => {
+        if (!isMoving) {
+          const currentTime = backgroundAudioManager.currentTime
+          const duration = backgroundAudioManager.duration
+          const sec = currentTime.toString().split('.')[0]
+          if (sec != currentSec) {
+            const currentTimeFmt = this._dateFormat(currentTime)
+            this.setData({
+              movableDis: (movableAreaWidth - movableViewWidth) * currentTime / duration,
+              progress: currentTime / duration * 100,
+              ['showTime.currentTime']: `${currentTimeFmt.min}:${currentTimeFmt.sec}`,
+            })
+            currentSec = sec
+            // 联动歌词
+            this.triggerEvent('timeUpdate', {
+              currentTime
+            })
+          }
+        }
+      })
+
+      backgroundAudioManager.onEnded(() => {
+        console.log("onEnded")
+        this.triggerEvent('musicEnd')
+      })
+
+      backgroundAudioManager.onError((res) => {
+        console.error(res.errMsg)
+        console.error(res.errCode)
+        wx.showToast({
+          title: '错误:' + res.errCode,
+        })
+      })
+    },
+
+    _setTime() {
+      duration = backgroundAudioManager.duration
+      const durationFmt = this._dateFormat(duration)
+      this.setData({
+        ['showTime.totalTime']: `${durationFmt.min}:${durationFmt.sec}`
+      })
+    },
+   
+    _dateFormat(sec) {
+      const min = Math.floor(sec / 60)
+      sec = Math.floor(sec % 60)
+      return {
+        'min': this._parse0(min),
+        'sec': this._parse0(sec),
+      }
+    },
+
+    _parse0(sec) {
+      return sec < 10 ? '0' + sec : sec
+    }
+
+  }
+})
