c80f4770b48228606cd77a0430a6f6f1c21db29d
xukee <oncwnuBDYPJsFlWf_CezM2n3uqDU@git.weixin.qq.com>
2020-07-14 11:25:43 +0800

    最忌播放

--- miniprogram/pages/playhistory/playhistory.js
+++ miniprogram/pages/playhistory/playhistory.js
@@ -19,58 +19,16 @@ Page({
     //获取历史播放 判断当前歌曲是否在历史列表数据里
     const history = wx.getStorageSync(openid)
 
-    this.setData({
-      musiclist: history
-    })
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    if (history.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '暂无历史记录',
+      })
+    } else {
+      this.setData({
+        musiclist: history
+      })
+    }
   }
+  
 })
